2520561a05269c7a7475ff8a5b86cf912daf484e
ThePhyrus <romaisgood@yandex.ru>
2022-05-26 14:23:56 +0300

    Урок 1 | Познакомились с coordinator layout & BottomSheetBehavior.

--- app/src/main/java/rom/example/pictureoftheday/view/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/rom/example/pictureoftheday/view/picture/PictureOfTheDayFragment.kt
@@ -3,6 +3,7 @@ package rom.example.pictureoftheday.view.picture
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,8 +11,10 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.load
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import rom.example.pictureoftheday.R
 import rom.example.pictureoftheday.databinding.FragmentPictureOfTheDayBinding
+import rom.example.pictureoftheday.utils.TAG
 import rom.example.pictureoftheday.utils.WIKI_DOMAIN
 import rom.example.pictureoftheday.viewmodel.PictureOfTheDayAppState
 import rom.example.pictureoftheday.viewmodel.PictureOfTheDayViewModel
@@ -20,7 +23,7 @@ import rom.example.pictureoftheday.viewmodel.PictureOfTheDayViewModel
 class PictureOfTheDayFragment : Fragment() {
 
     private var _binding: FragmentPictureOfTheDayBinding? = null
-    private val binding :FragmentPictureOfTheDayBinding get() = _binding!!
+    private val binding: FragmentPictureOfTheDayBinding get() = _binding!!
 
 
     override fun onCreateView(
@@ -31,7 +34,7 @@ class PictureOfTheDayFragment : Fragment() {
         return binding.root
     }
 
-    private val viewModel:PictureOfTheDayViewModel by lazy {
+    private val viewModel: PictureOfTheDayViewModel by lazy {
         ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
     }
 
@@ -50,17 +53,51 @@ class PictureOfTheDayFragment : Fragment() {
         }
     }
 
-    private fun renderData(pictureOfTheDayAppState: PictureOfTheDayAppState){
-        when(pictureOfTheDayAppState){
+    private fun renderData(pictureOfTheDayAppState: PictureOfTheDayAppState) {
+        when (pictureOfTheDayAppState) {
             is PictureOfTheDayAppState.Error -> {}//todo
             is PictureOfTheDayAppState.Loading -> {}//todo
             is PictureOfTheDayAppState.Success -> {
-                binding.imageView.load(pictureOfTheDayAppState.pictureOfTheDayResponseData.url)
-                //todo HW скрасить ожидание картинки
+                with(binding) {
+                    imageView.load(pictureOfTheDayAppState.pictureOfTheDayResponseData.url)
+                    lifeHack.title.text =
+                        pictureOfTheDayAppState.pictureOfTheDayResponseData.title
+                    lifeHack.explanation.text =
+                        pictureOfTheDayAppState.pictureOfTheDayResponseData.explanation
+                    //todo HW скрасить ожидание картинки
+                }
             }
         }
 
+
+        val bottomShiftBehavior = BottomSheetBehavior.from(binding.lifeHack.bottomSheetContainer)
+        bottomShiftBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        bottomShiftBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_DRAGGING -> {}
+                    BottomSheetBehavior.STATE_COLLAPSED -> {}
+                    BottomSheetBehavior.STATE_EXPANDED -> {}
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {}
+                    BottomSheetBehavior.STATE_HIDDEN -> {}
+                    BottomSheetBehavior.STATE_SETTLING -> {}
+                }
+                Log.d(
+                    TAG,
+                    "onStateChanged() called with: bottomSheet = $bottomSheet, newState = $newState"
+                )
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                Log.d(
+                    TAG,
+                    "onSlide() called with: bottomSheet = $bottomSheet, slideOffset = $slideOffset"
+                )
+            }
+        })
     }
+
 
     companion object {
         @JvmStatic
