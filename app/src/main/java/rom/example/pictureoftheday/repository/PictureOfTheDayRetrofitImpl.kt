7733651392bbed8185139fe41c249ea330904c4e
ThePhyrus <romaisgood@yandex.ru>
2022-05-27 15:16:43 +0300

    Урок 2 | Вынес создание Retrofit из getRetrofit().

--- app/src/main/java/rom/example/pictureoftheday/repository/PictureOfTheDayRetrofitImpl.kt
+++ app/src/main/java/rom/example/pictureoftheday/repository/PictureOfTheDayRetrofitImpl.kt
@@ -6,13 +6,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 import rom.example.pictureoftheday.utils.NASA_DOMAIN
 
 class PictureOfTheDayRetrofitImpl {
+    //FIXME правильно ли я вынес создание Retrofit?
     private val nasaBaseUrl: String = NASA_DOMAIN
+    private val pictureOfTheDayRetrofit = Retrofit.Builder()
+        .baseUrl(nasaBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
+        .build()
 
-    fun getRetrofit(): PictureOfTheDayAPI {//FIXME вынести создание Retrofit
-        val pictureOfTheDayRetrofit = Retrofit.Builder()
-            .baseUrl(nasaBaseUrl)
-            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
-            .build()
+    fun getRetrofit(): PictureOfTheDayAPI {
         return pictureOfTheDayRetrofit.create(PictureOfTheDayAPI::class.java)
     }
 }
